7877dc9aa3f61172c61854f2eb897b48c1084259
MathieuMac <mathieusoufflard@gmail.com>
2021-06-09 11:24:52 +0200

    white button become a Yello one, all color btn are clickable and add there color on the sting sequece.

--- Mobile/app/src/main/java/com/example/xbee_simon/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/xbee_simon/MainActivity.kt
@@ -2,13 +2,41 @@ package com.example.xbee_simon
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import com.example.xbee_simon.databinding.ActivityMainBinding
+
+private const val TAG = "MainActivity"
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        Log.i("message", binding.SequenceTxt.text.toString())
+        binding.yellowBtn.setOnClickListener{
+            addSequece("Yellow")
+        }
+        binding.greenBtn.setOnClickListener{
+            addSequece("Green")
+        }
+
+        binding.redBtn.setOnClickListener{
+            addSequece("Red")
+        }
 
 
+    }
+
+    private fun addSequece(s: String) {
+
+        Log.i("message", s)
+
+        binding.SequenceTxt.text = binding.SequenceTxt.text.toString() + s + ","
     }
 
 
