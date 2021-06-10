b9d8a3976c238cd1eccee344a254900aa101d39b
MathieuMac <mathieusoufflard@gmail.com>
2021-06-10 23:23:29 +0200

    try to change data in db

--- Mobile/app/src/main/java/com/example/xbee_simon/MainActivity.kt
+++ Mobile/app/src/main/java/com/example/xbee_simon/MainActivity.kt
@@ -1,11 +1,11 @@
 package com.example.xbee_simon
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
 import com.example.xbee_simon.databinding.ActivityMainBinding
-import com.google.firebase.analytics.FirebaseAnalytics
 import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.IgnoreExtraProperties
 import com.google.firebase.database.ktx.database
 import com.google.firebase.ktx.Firebase
@@ -19,18 +19,13 @@ class MainActivity : AppCompatActivity() {
     private var tippingSequence = 0
     private var canAddSequence = true
     private var sequence = ""
-    private lateinit var database: DatabaseReference
-
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-            binding.sequenceSizeTxt.text = binding.sequenceSizeTxt.text.toString() + nbSequence
-            database = Firebase.database.reference
-
+        binding.sequenceSizeTxt.text = binding.sequenceSizeTxt.text.toString() + nbSequence
 
         binding.blueBtn.setOnClickListener {
                 if(canAddSequence) {
@@ -58,7 +53,6 @@ class MainActivity : AppCompatActivity() {
         sequence = sequence + s + ","
         if(tippingSequence == nbSequence) {
             canAddSequence = false;
-            Log.i("sequence", sequence)
 
             writeSequence(java.util.Calendar.getInstance().time, sequence)
         }
@@ -66,18 +60,16 @@ class MainActivity : AppCompatActivity() {
 
     private fun writeSequence(date: Date, sequence: String) {
 
-        Log.i("datatest", sequence)
-        Log.i("datatest", "coucou")
+        var database = FirebaseDatabase.getInstance()
 
-        val data = DataSequence(date, sequence)
 
-        try {
-            database.child("sequence").child("1").setValue(data)
-            Log.i("trydata", "on est la")
-       }
-        catch (ex: Exception){
-            Log.i("catch", ex.toString())
-        }
+
+        val myRef = database.getReference("sequence").keepSynced(true)
+
+        val dataSequence = DataSequence(date, sequence)
+        database.child("sequence").child("0").setValue(myRef)
+
+        Log.i("toto", sequence)
     }
 }
 
@@ -86,3 +78,4 @@ data class DataSequence(val date: Date? = null, val sequence: String? = null) {
     // Null default values create a no-argument default constructor, which is needed
     // for deserialization from a DataSnapshot.
 }
+
